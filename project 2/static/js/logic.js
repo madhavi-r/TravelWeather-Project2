ff4b2397dc4a395bb495935e94de21d55e7dd989
Mad Rag <40861598+madhavi-r@users.noreply.github.com>
2018-12-15 15:37:16 -0500

    updated files

--- project 2/static/js/logic.js
+++ project 2/static/js/logic.js
@@ -1,12 +1,61 @@
+var all_data = "/forecast";
 
-var alldata = "/forecast";
-
-d3.json(alldata,function(data){
+d3.json(all_data, function(data){
   console.log(data);
 
+      mapboxgl.accessToken = API_KEY;
 
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [-95.71, 37.09],
+        zoom: 3.5
+    });
 
+    var geocoder = new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken
+    });
 
+    map.addControl(geocoder);
+
+    // After the map style has loaded on the page, add a source layer and default
+    // styling for a single point.
+    map.on('load', function() {
+        map.addSource('single-point', {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": []
+            }
+        });
+
+        map.addLayer({
+            "id": "point",
+            "source": "single-point",
+            "type": "circle",
+            "paint": {
+                "circle-radius": 10,
+                "circle-color": "#007cbf"
+            }
+        });
+
+        // Listen for the `result` event from the MapboxGeocoder that is triggered when a user
+        // makes a selection and add a symbol that matches the result.
+        geocoder.on('result', function(ev) {
+            map.getSource('single-point').setData(ev.result.geometry);
+        });
+    });
+
+    data.forEach(function(marker){
+      var el = document.createElement('div');
+      el.className = "marker";
+      var coords = [marker.lon, marker.lat];
+      new mapboxgl.Marker(el)
+        .setLngLat(coords)
+        .setPopup(new mapboxgl.Popup({ offset: 25 })
+        .setHTML('<h3>' + marker.fcst_avg + '</h3>'))
+        .addTo(map);
+    }) 
 });
 
 
@@ -33,103 +82,5 @@ d3.json(alldata,function(data){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// d3.csv("northamerica_station_information.csv", function(data) {
-//   console.log(data);
-
-//     var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-//       attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//       maxZoom: 18,
-//       id: "mapbox.streets",
-//       accessToken: API_KEY
-//     });
-//     var baseMaps = {
-//       "Street Map": streetmap,
-//     };
-//     /*
-//     var overlayMaps = {
-//       Markers: marker
-//     };
-//     */
-//     var myMap = L.map("map", {
-//       center: [
-//         37.09, -95.71
-//       ],
-//       zoom: 3.5,
-//       layers: [streetmap, /*station*/]
-//     });
-//     L.control.layers(baseMaps, {
-//       //collapsed: false
-//     }).addTo(myMap);
-
-//   /*
-//   var lat = []
-//   var lng = []
-//   data.forEach((obs) => {
-//     Object.entries(obs).forEach(function([key,value]) {
-//         if (key === "lat") {
-//             lat.push(value)
-//         }
-//         if (key === "lon") {
-//             lng.push(value);
-//         }         
-//     });
-//   });
-//   //console.log(lat)
-//   //console.log(lng)
-//   */
-
-//   for (i = 0; i < data.length; i++) { 
-//     var marker = L.marker([data[i].lat, data[i].lon]).addTo(myMap);;
-//   }
-  
-  
-// });
-// /*
-
-
-// function createFeatures(earthquakeData) {
-//   function onEachFeature(feature, layer) {
-//     layer.bindPopup("<h3>" + feature.properties.place +
-//       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
-//       "<p> Magnitude: " + feature.properties.mag + "</p>");
-//   }
-
-//   function circleStyling(feature) {
-//   	return {
-//       color: "black",
-//       radius: feature.properties.mag * 4,
-//       fillColor: magColor(feature.properties.mag),
-//       fillOpacity: 1,
-//       weight: 0.75,
-//   	};
-//   }
-//   function pointToLayer(feature, coord) {
-//     return L.circleMarker(coord, circleStyling(feature)); 
-//   }
-
-//   var earthquakes = L.geoJSON(earthquakeData, {
-//     pointToLayer:pointToLayer,
-//     onEachFeature: onEachFeature
-//   });
-//   createMap(earthquakes);
-// }
-
-
-// */
 
 
